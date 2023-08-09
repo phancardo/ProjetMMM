c9b87493c23b17bbe31d12bfb2d5c293dafae36c
phancardo <andrianiainarabe@gmail.com>
2023-08-09 13:21:41 +0300

    aona

--- server/src/main/java/com/example/server/repository/DistrictRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.repository;
-
-import com.example.server.model.District;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DistrictRepository extends JpaRepository<District, Integer> {
-}
